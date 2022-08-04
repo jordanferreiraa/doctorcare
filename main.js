2b4ccdc45781a8766acfc358fbb44a06aecbe369
Jordan Ferreira <jordanferreira69@gmail.com>
2022-08-04 16:15:20 -0300

    update nlw2

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function onScroll() {
-  navigation.classList.add('scroll')
+  const elemento = document.getElementById("navigation")
+    if(scrollY > 0) {
+        elemento.classList.add("scroll")
+    }else{
+        elemento.classList.remove("scroll")
+    }
+}
+
+function openMenu() {
+  document.body.classList.add("menu-expanded")
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded")
 }
