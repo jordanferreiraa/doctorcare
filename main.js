ed63b1637df7fa9a4311adf5347ced8dc7f33ef1
Jordan Ferreira <jordanferreira69@gmail.com>
2022-09-13 14:52:38 -0300

    finished

--- main.js
+++ main.js
@@ -5,6 +5,41 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  //verificar se a seção passou da linha alvo
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  //o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+  
+  //verificar se a base está abaixo da linha alvo
+  //a seção termina onde ?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  //o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  //limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+
 }
 
 function showNavOnScroll() {
